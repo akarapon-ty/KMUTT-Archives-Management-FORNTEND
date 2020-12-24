55142c7ec263428dd8bd7293eb2de1477b97d0d6
akarapon.ty <33413377+beraba@users.noreply.github.com>
2020-12-24 21:34:28 +0700

    edit routing remove check login

--- src/routing.js
+++ src/routing.js
@@ -3,30 +3,24 @@ import {
   Route, BrowserRouter, Switch, Redirect,
 } from 'react-router-dom'
 
-import { AuthInContext } from './store/actions/auth'
 import NavigationsItems from './components/navigationItems/NavigationsItems'
 import Test from './pages/test'
 
 const Login = React.lazy(() => import('./pages/login'))
 const Homepage = React.lazy(() => import('./pages/homepage'))
 
-const Router = () => {
-  const { loggedIn } = useContext(AuthInContext)
-
-  return (
-    <BrowserRouter>
-      <Suspense fallback={<span>Loading....</span>}>
-        <NavigationsItems />
-        <Switch>
-          <Route path="/homepage" component={Homepage} />
-          <Route path="/test" component={Test} />
-          <Route path="/login" component={Login} />
-          <Redirect to="/homepage" />
-        </Switch>
-        {/* {loggedIn ? null : <Redirect to="/login" />} */}
-      </Suspense>
-    </BrowserRouter>
-  )
-}
+const Router = () => (
+  <BrowserRouter>
+    <Suspense fallback={<span>Loading....</span>}>
+      <NavigationsItems />
+      <Switch>
+        <Route path="/homepage" component={Homepage} />
+        <Route path="/test" component={Test} />
+        <Route path="/login" component={Login} />
+        <Redirect to="/homepage" />
+      </Switch>
+    </Suspense>
+  </BrowserRouter>
+)
 
 export default Router
